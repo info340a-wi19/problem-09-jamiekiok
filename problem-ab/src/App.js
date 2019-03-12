837264d4f31c292b0508d792ed4074ecff4c413a
Jamie Walker <jamiekw@uw.edu>
2019-03-11 18:17:32 -0700

    passes all tests for prob a :)

--- problem-ab/src/App.js
+++ problem-ab/src/App.js
@@ -9,31 +9,31 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {loading:true};
+
+    this.handleSignIn = this.handleSignIn.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.handleSignOut = this.handleSignOut.bind(this);
 }
 
   //A callback function for registering new users
   handleSignUp = (email, password, handle, avatar) => {
-    // this.setState({errorMessage:null}); //clear any old errors
-    this.state.errorMessage = null;
-
-
+    this.setState({errorMessage:null}); //clear any old errors
 
     /* TODO: sign up user here */
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredentials) => {
         // note to self: remember to initalize this to use it!!!
         let user = userCredentials.user; //access the newly created user
-        console.log('User created: '+user.uid);
         user.updateProfile({
           displayName : handle,
           photoURL : avatar
         }).catch((error) => {
           // An error happened.
-          this.state.errorMessage = error.message;
+          this.setState({errorMessage: error.message});
         }); 
       })
       .catch((error) => { //report any errors
-        this.state.errorMessage = error.message;
+        this.setState({errorMessage: error.message});
       });
     }
 
@@ -41,8 +41,6 @@ class App extends Component {
     this.authUnSubFunction = firebase.auth().onAuthStateChanged((firebaseUser) => {
       if(firebaseUser) { //if exists, then we logged in
         let user = firebaseUser;
-        console.log('made it to component did mount:');
-        console.log(user);
         this.setState({user: user});
       } else {
         this.setState({user: null});
@@ -57,24 +55,13 @@ class App extends Component {
 
   //A callback function for logging in existing users
   handleSignIn = (email, password) => {
-    console.log('this state:')
-    console.log(this.state);
-    console.log('this');
-    console.log(this);
-    this.state.test = true; //works but very bad style
-    this.state.errorMessage = null; //works but very bad style
-    // this.setState({test:true}); 
-    // this.setState({errorMessage:null}); //clear any old errors
+    this.setState({test:true}); 
+    this.setState({errorMessage:null}); //clear any old errors
     
-    console.log('this state 2 :') // setting but has error
-    console.log(this.state);
-
     /* TODO: sign in user here */
     firebase.auth().signInWithEmailAndPassword(email, password)
       .catch((err) => {  //log any errors for debugging
-        this.state.errorMessage = err.message;
-        //this.setState({errorMessage: err.message})
-        console.log(this.state.errorMessage);
+        this.setState({errorMessage: err.message})
        })
    }
 
